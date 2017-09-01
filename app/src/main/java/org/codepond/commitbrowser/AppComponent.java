8dc218d9f3086339ee2685a9d692ad5db5b38538
Nimrod Dayan <nimrod@codepond.org>
2017-09-01 11:39:23 +0300

    Initial Dagger set up

---
+++ app/src/main/java/org/codepond/commitbrowser/AppComponent.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 Nimrod Dayan CodePond.org
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codepond.commitbrowser;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+/**
+ * Application component refers to application level modules only
+ */
+@Component(modules = {
+        AndroidSupportInjectionModule.class,
+        AppModule.class,
+        BuildersModule.class })
+public interface AppComponent {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance Builder application(App application);
+        AppComponent build();
+    }
+    void inject(App app);
+}
---
+++ app/src/main/java/org/codepond/commitbrowser/commitlist/CommitListActivity.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 Nimrod Dayan CodePond.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codepond.commitbrowser.commitlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.codepond.commitbrowser.R;
+
+import dagger.android.AndroidInjection;
+
+public class CommitListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.commit_list);
+    }
+}
