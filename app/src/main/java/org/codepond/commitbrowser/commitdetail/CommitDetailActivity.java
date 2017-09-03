d40ed135a561bd1b946d727af787dd2128ad5c60
Nimrod Dayan <nimrod@codepond.org>
2017-09-04 01:27:51 +0300

    Removed fragment

--- app/src/main/java/org/codepond/commitbrowser/commitdetail/CommitDetailActivity.java
+++ app/src/main/java/org/codepond/commitbrowser/commitdetail/CommitDetailActivity.java
@@ -1,11 +1,7 @@
 package org.codepond.commitbrowser.commitdetail;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import org.codepond.commitbrowser.R;
 
@@ -17,18 +13,7 @@ public class CommitDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.commit_detail_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        String sha = getIntent().getStringExtra(EXTRA_COMMIT_SHA);
     }
-
 }
