38c4b853405182b5acba6e3714254061f303df6b
Nimrod Dayan <nimrod.dayan@gmail.com>
2019-04-27 21:05:57 +0300

    Updated kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2019 Nimrod Dayan nimroddayan.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 buildscript {
     repositories {
         google()
@@ -40,7 +56,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.0")
-        classpath(kotlin("gradle-plugin", version = "1.3.21"))
+        classpath(kotlin("gradle-plugin", version = "1.3.31"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.0.0")
         classpath("org.jmailen.gradle:kotlinter-gradle:1.23.1")
     }
